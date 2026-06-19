d6821c742f8dfb7a67901b7e9c6b2bf591d94dec
tamayo <187291746+cubalibre59@users.noreply.github.com>
2026-06-19 17:50:24 +0200

    Ajout de la page deco sans supprimer les guides SaaS

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main:         resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, 'index.html'),
         guideGratuit: resolve(__dirname, 'guide-gratuit.html'),
-        guidePro:     resolve(__dirname, 'guide-pro.html'),
+        guidePro: resolve(__dirname, 'guide-pro.html'),
+        deco: resolve(__dirname, 'deco/index.html'),
       }
     }
   }
